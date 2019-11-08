2c87537ac88ca5048981e8b2c33f861e5dbc234e
Hiyorimi <playittodeath@gmail.com>
2019-11-08 18:00:41 +0300

    Conflict resolved: altered quotes to double-quotes

--- django/example/settings.py
+++ django/example/settings.py
@@ -51,9 +51,9 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [],
         "APP_DIRS": True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
         },
